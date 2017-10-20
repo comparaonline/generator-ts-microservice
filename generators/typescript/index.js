0796998fbb0a09abab43719cf4e25f79328cd6b7
Ezequiel Rabinovich <erabinovich@gmail.com>
2017-10-20 16:52:18 -0300

    Add typescript generator

---
+++ generators/typescript/index.js
@@ -0,0 +1,35 @@
+'use strict';
+const _ = require('lodash');
+const extend = _.merge;
+const mkdirp = require('mkdirp');
+const Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+  writing() {
+    mkdirp('src');
+    mkdirp('build');
+    this.fs.copy(
+      this.templatePath('tsconfig.json'),
+      this.destinationPath('tsconfig.json')
+    );
+
+    const currentPkg = this.fs.readJSON(this.destinationPath('package.json'), {});
+
+    const pkg = extend({
+      scripts: {
+        compile: 'tsc --outDir ./build --module commonjs ./src/*.ts',
+        prestart: 'npm run compile',
+        start: 'node ./build',
+        watch: 'nodemon -e ts -w ./src -x npm run watch:serve',
+        'watch:serve': 'ts-node --inspect src/index.ts'
+      }
+    }, currentPkg);
+
+    this.fs.writeJSON(this.destinationPath('package.json'), pkg);
+  }
+
+  install() {
+    this.npmInstall(['typescript']);
+    this.npmInstall(['ts-node'], { 'save-dev': true });
+  }
+};
