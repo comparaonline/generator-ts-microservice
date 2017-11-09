2c53a181a6a95816654725b9913b86cf8c5ba44c
Joaquin Moreira <jmoreira@comparaonline.com>
2017-11-09 16:41:01 -0300

    Add express generator and make it the default

--- generators/node-config/index.js
+++ generators/node-config/index.js
@@ -5,12 +5,24 @@ const mkdirp = require('mkdirp');
 const Generator = require('yeoman-generator');
 
 module.exports = class extends Generator {
+  constructor(args, options) {
+    super(args, options);
+    this.option('name', {
+      type: String,
+      required: true,
+      desc: 'Project name'
+    });
+  }
+
   writing() {
     mkdirp.sync('config');
     const envs = ['default', 'development', 'qc', 'staging', 'production'];
-    envs.forEach(env => this.fs.copy(
+    envs.forEach(env => this.fs.copyTpl(
       this.templatePath(`${env}.json`),
-      this.destinationPath(`config/${env}.json`)
+      this.destinationPath(`config/${env}.json`),
+      {
+        microserviceName: this.options.name
+      }
     ));
   }
 
