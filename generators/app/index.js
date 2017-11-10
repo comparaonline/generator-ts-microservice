d5261b663bcfdba570a023b33f78acd704b3f701
Ezequiel Rabinovich <erabinovich@gmail.com>
2017-11-10 14:25:13 -0300

    Add optional sequelize dependency

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,6 +4,8 @@ const chalk = require('chalk');
 const mkdirp = require('mkdirp');
 const Generator = require('yeoman-generator');
 const yosay = require('yosay');
+const _ = require('lodash');
+const extend = _.merge;
 
 module.exports = class extends Generator {
   initializing() {
@@ -12,29 +14,32 @@ module.exports = class extends Generator {
 
   prompting() {
     this.log(yosay(`Let's setup your new ${chalk.red('ComparaOnline')} microservice!`));
-
-    return this.prompt({
-      name: 'name',
-      message: 'Your microservice name',
-      default: path.basename(process.cwd()),
-      filter: name => name.toLowerCase().replace(/\s+|_/, '-')
-    }, this).then(props => {
-      this.props.name = props.name;
-      });
+    const prompts = [
+      {
+        name: 'name',
+        message: 'Your microservice name',
+        default: path.basename(process.cwd()),
+        filter: name => name.toLowerCase().replace(/\s+|_/, '-')
+      },
+      {
+        name: 'optionalDependencies',
+        message: 'Select which optional dependencies you need installed',
+        type: 'checkbox',
+        choices: [
+          {name: 'Sequelize ORM', value: '../sequelize'}
+        ]
+      }
+    ];
+    return this.prompt(prompts).then(props => {
+      this.props = extend(this.props, props);
+    });
   }
 
   default() {
     this.props.githubAccount = 'comparaonline';
     this.props.keywords = 'comparaonline, microservice'
 
-    if (path.basename(this.destinationPath()) !== this.props.name) {
-      this.log(
-        `Your microservice must be inside a folder named ${this.props.name}\n` +
-        "I'll automatically create this folder."
-      );
-      mkdirp.sync(this.props.name);
-      this.destinationRoot(this.destinationPath(this.props.name));
-    }
+    this._checkPath();
     const dependencies = [
       '../node',
       '../jest',
@@ -48,8 +53,20 @@ module.exports = class extends Generator {
       'generator-node/generators/git'
     ];
     dependencies
+      .concat(this.props.optionalDependencies)
       .map(require.resolve)
       .forEach(dependency => this.composeWith(dependency, this.props));
+  }
+
+  _checkPath() {
+    if (path.basename(this.destinationPath()) !== this.props.name) {
+      this.log(
+        `Your microservice must be inside a folder named ${this.props.name}\n` +
+        "I'll automatically create this folder."
+      );
+      mkdirp.sync(this.props.name);
+      this.destinationRoot(this.destinationPath(this.props.name));
+    }
   }
 
   install() {
---
+++ generators/sequelize/index.js
@@ -0,0 +1,26 @@
+'use strict';
+const _ = require('lodash');
+const extend = _.merge;
+const Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+  writing() {
+    const currentPkg = this.fs.readJSON(this.destinationPath('package.json'), {});
+
+    const pkg = extend({
+      scripts: {
+        sequelize: 'sequelize'
+      }
+    }, currentPkg);
+
+    this.fs.writeJSON(this.destinationPath('package.json'), pkg);
+  }
+
+  install() {
+    this.yarnInstall([ 'sequelize', 'sequelize-cli', 'sequelize-typescript' ]);
+  }
+
+  end() {
+    return this.spawnCommand('yarn', ['sequelize', 'init'])
+  }
+};
