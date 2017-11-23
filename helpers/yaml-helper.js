ac2955375f109e9b556a1a1c808d1c9ccd772b52
Ezequiel Rabinovich <erabinovich@gmail.com>
2017-11-23 15:51:28 -0300

    Add kubernetes config (#14)
    * Fix error with null lines
    
    * Add option for parsing yaml with templates
    
    * Add kubernetes generator
    
    * Bump version

--- helpers/yaml-helper.js
+++ helpers/yaml-helper.js
@@ -6,6 +6,17 @@ module.exports = (object) => {
   object.readYaml = function (path, defaultValue) {
     try {
       const text = this.fs.read(path);
+      return this.parseYaml(text);
+    } catch (e) {
+      if (defaultValue !== undefined) {
+        return defaultValue;
+      }
+      throw e;
+    }
+  };
+
+  object.parseYaml = (text, defaultValue) => {
+    try {
       return safeLoad(text);
     } catch (e) {
       if (defaultValue !== undefined) {
@@ -25,7 +36,8 @@ module.exports = (object) => {
   };
 
   object.extendYaml = function (originalPath, newPath, additionalYaml = {}) {
-    const originalYaml = this.readYaml(originalPath, {});
+    const originalYaml = typeof originalPath == 'string' ?
+      this.readYaml(originalPath, {}) : originalPath;
     const newYaml = typeof newPath === 'string' ?
       this.readYaml(newPath, {}) : newPath;
     const extendedYaml = extend(newYaml, additionalYaml, originalYaml);
