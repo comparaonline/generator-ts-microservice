1de02a472c9c8d8461d29cf62648818411256e9f
Ezequiel Rabinovich <erabinovich@gmail.com>
2018-09-06 17:08:25 -0300

    Fix KafkaServer initialization

--- generators/event-streamer/templates/event-server/index.ts
+++ generators/event-streamer/templates/event-server/index.ts
@@ -2,20 +2,4 @@ import { KafkaServer } from '@comparaonline/event-streamer';
 import * as config from 'config';
 import { router } from './router';
 
-export const server = new KafkaServer(router, config.get('kafka'));
-
-const start = () => Promise.resolve(server.start())
-  .then(() => 'KafkaServer started');
-
-const stop = () => new Promise((resolve, reject) => {
-  server.stop()
-    .catch((error: Error) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve('KafkaServer stopped');
-      }
-    });
-});
-
-export default { start, stop };
+export const kafkaServer = new KafkaServer(router, config.get('kafka'));
