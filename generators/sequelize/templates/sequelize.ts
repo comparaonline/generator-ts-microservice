d238e5811d30e8e3e91fe7dcf81149d3869326ca
Ezequiel Rabinovich <erabinovich@gmail.com>
2018-09-06 18:30:51 -0300

    Update sequelize loading paths

--- generators/sequelize/templates/sequelize.ts
+++ generators/sequelize/templates/sequelize.ts
@@ -6,5 +6,10 @@ const extend = (...objs) => Object.assign({}, ...objs);
 
 export const sequelize = new Sequelize(extend(
   config.get('sequelize'),
-  { modelPaths: [path.join(__dirname, '../models')] }
+  {
+    modelPaths: [
+      path.join(__dirname, '../models/**/*.model.js'),
+      path.join(__dirname, '../models/**/*.model.ts')
+    ]
+  }
 ));
