5c6f7e96dc5858652b5b47bcb14ad839a66a7d1b
Esteban Martini <esteban.martini@gmail.com>
2018-09-11 20:25:32 -0300

    replace Object.keys().map by Object.keys

--- generators/mocha/templates/sequelize.ts
+++ generators/mocha/templates/sequelize.ts
@@ -1,7 +1,6 @@
 import { sequelize } from '../initialization/sequelize';
 
-const models = Object.keys(sequelize.models)
-  .map(key => sequelize.models[key]);
+const models = Object.values(sequelize.models);
 
 beforeEach(async () => Promise.all(models.map(model =>
   model.truncate({ cascade: true })
