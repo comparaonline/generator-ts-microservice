4f8fc4129a3b0ccbade53ac238dfb1dd5825403b
Juan Francisco Lobos <jflobos@gmail.com>
2019-04-02 16:22:40 -0300

    Fix datadog APM configuration

--- generators/datadog/templates/datadog.ts
+++ generators/datadog/templates/datadog.ts
@@ -1,15 +1,31 @@
-import tracer from 'dd-trace';
+import TraceProxy, { TracerOptions } from 'dd-trace';
 import * as config from 'config';
+import * as opentracing from 'opentracing';
 
-if (config.util.getEnv('DD_AGENT_HOST')) {
-  tracer.init({
-    service: config.get('appName'),
-    hostname: config.util.getEnv('DD_AGENT_HOST'),
-    env: config.util.getEnv('NODE_CONFIG_ENV'),
-    tags: {
-      environment: config.util.getEnv('NODE_CONFIG_ENV'),
-      project: config.get('datadog.projectName'),
-      serviceName: config.get('appName')
-    }
-  });
-}
+const baseConfig: TracerOptions = {
+  service: config.get('appName'),
+  env: process.env.NODE_CONFIG_ENV,
+  tags: {
+    environment: process.env.NODE_CONFIG_ENV,
+    project: config.get('appName'),
+    serviceName: config.get('appName')
+  }
+};
+
+const getDatadogConfig = (): TracerOptions => {
+  if (!process.env.DD_AGENT_HOST) {
+    return {
+      ...baseConfig,
+      debug: false,
+      hostname: config.get('datadog.host')
+    };
+  }
+  return {
+    ...baseConfig,
+    hostname: process.env.DD_AGENT_HOST
+  };
+};
+
+const tracer = TraceProxy.init(getDatadogConfig());
+
+opentracing.initGlobalTracer(tracer);
