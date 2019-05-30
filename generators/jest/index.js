f61d1b3db1572cd9d32071b366979c9226b13109
Ezequiel Rabinovich <erabinovich@gmail.com>
2019-05-30 14:04:59 -0400

    Fixes for jest and mocha

--- generators/jest/index.js
+++ generators/jest/index.js
@@ -1,6 +1,7 @@
 'use strict';
 const Generator = require('yeoman-generator');
 const addScript = require('../../helpers/add-script');
+const mkdirp = require('mkdirp');
 
 module.exports = class extends Generator {
   static get devDependencies() {
@@ -8,10 +9,50 @@ module.exports = class extends Generator {
   }
   writing() {
     addScript(this, 'test', 'jest');
+    addScript(this, 'test:fast', 'jest');
+    addScript(this, 'pretest', this._preTest())
 
-    this.fs.copy(
-      this.templatePath('jest.config.js'),
-      this.destinationPath('jest.config.js')
+    this.fs.copyTpl(
+      this.templatePath('jest.config.js.ejs'),
+      this.destinationPath('jest.config.js'),
+      { additional: this._additional() }
     );
+
+    this._testInitialization();
+  }
+
+  _orms() {
+    return ['sequelize', 'typeorm']
+      .filter(orm => this.options.hasDependency(orm));
+  }
+
+  _additional() {
+    return ['sequelize', 'typeorm']
+      .filter(orm => this.options.hasDependency(orm))
+      .map(orm => `"./src/test-helpers/${orm}.ts"`)
+      .join(',\n    ');
+  }
+
+  _preTest() {
+    return Object.entries({
+      tslint: 'yarn tslint',
+      sequelize: 'NODE_CONFIG_ENV=test yarn migrate',
+      typeorm: 'NODE_CONFIG_ENV=test yarn migrate'
+    })
+      .filter(([k]) => this.options.hasDependency(k))
+      .map(([_, v]) => v)
+      .join(' && ');
+  }
+
+  _testInitialization() {
+    const orms = this._orms();
+    if (orms.length === 0) return;
+    mkdirp.sync(this.destinationPath('src/test-helpers'));
+    orms
+      .map(orm => [`${orm}.ts`, `src/test-helpers/${orm}.ts`])
+      .forEach(([from, to]) => this.fs.copy(
+        this.templatePath(from),
+        this.destinationPath(to)
+      ));
   }
 };
---
+++ generators/jest/templates/sequelize.ts
@@ -0,0 +1,9 @@
+import { sequelize } from '../initialization/sequelize';
+
+const models = Object.values(sequelize._);
+
+beforeEach(async () => Promise.all(
+  Object.values(models).map((model: any) =>
+    model.truncate({ cascade: true })
+)));
+afterAll(() => sequelize.connectionManager.close());
