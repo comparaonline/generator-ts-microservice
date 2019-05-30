75e9337a87790fb0076f81768184aab73bb9256c
Ezequiel Rabinovich <erabinovich@gmail.com>
2019-05-30 12:11:31 -0400

    Fix ping tests

--- generators/event-streamer/templates/event-server/actions/__tests__/ping-action.mocha.ts
+++ generators/event-streamer/templates/event-server/actions/__tests__/ping-action.mocha.ts
@@ -6,8 +6,8 @@ import { Pong } from '../../events/ping-events';
 describe('PingAction', () => {
   it('returns consumer data when pinged', async () => {
     const server = new TestServer(router);
-    server.input({ code: 'Ping' });
-    const published = await server.emitted();
+    await server.input({ code: 'Ping' });
+    const published = server.emitted();
     expect(published).to.have.length(1);
     expect(published[0]).to.be.instanceof(Pong);
     const publishedData = JSON.parse(published[0].toString());
